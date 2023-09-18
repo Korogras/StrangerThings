081da87c3e8c9d41b2d6545b7b8656c55701d097
Korogras <brandon_faubion@yahoo.com>
2023-09-18 18:19:56 -0500

    first commit

---
+++ src/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>list</li>
+          <li>list</li>
+        </ul>
+      </nav>
+    </div>
+    </>
+  )
+}
+export default Navbar
---
+++ src/post.jsx
@@ -0,0 +1,5 @@
+import react from 'react'
+
+const COHORT_NAME = '2302-ACC-ET-WEB-PT-D'
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+
